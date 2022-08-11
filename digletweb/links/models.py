8b65071d3b06fc6b0e01b3e9375ed701438fbdc3
Kacper Stypik <kacperstypik@mailbox.org>
2022-08-11 17:22:07 +0200

    Add Link model

--- digletweb/links/models.py
+++ digletweb/links/models.py
@@ -1,3 +1,24 @@
+from autoslug import AutoSlugField
+from digletweb.users.models import User
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+def thumbnail_path(instance, filename):
+    try:
+        file_ext = filename.split(".")[-1]
+    except KeyError:
+        file_ext = ""
+    return f"{instance.slug}.{file_ext}"
+
+
+class Link(TimeStampedModel):
+    title = models.CharField(max_length=150)
+    slug = AutoSlugField(populate_from="title", unique=True)
+    domain = models.URLField()
+    description = models.TextField(max_length=1000)
+    thumbnail = models.ImageField(upload_to=thumbnail_path, blank=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
