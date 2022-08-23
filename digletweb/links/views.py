3faacd8999d6b99038390b84476b955bd363130f
Kacper Stypik <kacperstypik@mailbox.org>
2022-08-23 19:52:46 +0200

    Allow adding related links only for authenticated

--- digletweb/links/views.py
+++ digletweb/links/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from vote.views import VoteMixin
 
@@ -21,6 +22,7 @@ class LinkViewSet(viewsets.ModelViewSet, VoteMixin):
 class RelatedLinkListCreate(generics.ListCreateAPIView):
     serializer_class = RelatedLinkSerializer
     queryset = RelatedLink.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         return RelatedLink.objects.filter(parent=self.kwargs["id"])
